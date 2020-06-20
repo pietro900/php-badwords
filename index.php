6db2915a41cdec105ff0f3134f631546755375d5
pietro <pietro1990@hotmail.it>
2020-06-20 06:32:46 +0200

    col metodo get

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
+$parola_utente = $_GET['parola'];
 $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
 
-$parola_censurata = str_replace("Duis", "****", $testo);
+$parola_censurata = str_replace($parola_utente, "****", $testo);
 
-echo "$parola_censurata";
+echo $parola_censurata;
 
 echo strlen($testo);
 ?>
